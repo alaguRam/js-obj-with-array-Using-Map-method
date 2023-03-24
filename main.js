df6d3d7dd376b29e6785fccb653f95fa935a55af
alaguRam <alaguram393@gmail.com>
2023-03-24 16:46:29 +0530

    update

---
+++ main.js
@@ -0,0 +1,22 @@
+//Using Map method
+const users = [
+  {
+    first_name: 'Mike',
+    last_name: 'Sheridan'
+  },
+  {
+    first_name: 'Tim',
+    last_name: 'Lee'
+  },
+  {
+    first_name: 'John',
+    last_name: 'Carte'
+  }
+];
+
+
+let result=users.map(function(val)
+	{
+	return val.first_name+val.last_name;
+	})
+	console.log(result);
